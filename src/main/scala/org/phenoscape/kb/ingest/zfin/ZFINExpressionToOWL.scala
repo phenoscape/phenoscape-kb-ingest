8d302b6e6dade496cc3c7ec447b060e913f5d529
Jim Balhoff <jim@balhoff.org>
2016-01-18 12:58:29 -0500

    Added axiom example for ZFIN expression.

--- src/main/scala/org/phenoscape/kb/ingest/zfin/ZFINExpressionToOWL.scala
+++ src/main/scala/org/phenoscape/kb/ingest/zfin/ZFINExpressionToOWL.scala
@@ -14,8 +14,44 @@ import org.semanticweb.owlapi.model.OWLAxiom
 
 object ZFINExpressionToOWL {
 
+  /**
+   * @param expressionData tab-delimited input from http://zfin.org/downloads/wildtype-expression_fish.txt
+   * Each line (an annotation) is translated into a set of OWL axioms
+   * @return
+   */
   def convert(expressionData: Source): Set[OWLAxiom] = expressionData.getLines.flatMap(translate).toSet[OWLAxiom]
 
+  /**
+   * @param expressionLine
+   * Example line:
+   * ZDB-GENE-000125-4	dlc	AB/TU	ZFA:0000107	eye	\ 	\ 	Pharyngula:Prim-15	Pharyngula:Prim-25	mRNA in situ hybridization	ZDB-PUB-051025-1	ZDB-EST-051107-52		ZDB-FISH-150901-29084
+   * Should generate OWL:
+   *
+   * Prefix: GO: <http://purl.obolibrary.org/obo/GO_>
+   * Prefix: NCBITaxon: <http://purl.obolibrary.org/obo/NCBITaxon_>
+   * Prefix: ZFA: <http://purl.obolibrary.org/obo/ZFA_>
+   * Prefix: BFO: <http://purl.obolibrary.org/obo/BFO_>
+   * Prefix: dc: <http://purl.org/dc/terms/>
+   * Prefix: ps: <http://purl.org/phenoscape/vocab.owl#>
+   * Prefix: zfin: <http://zfin.org/>
+   * Prefix: uuid: <http://purl.org/phenoscape/uuid/>
+   * 
+   *
+   * Individual: uuid:033ab9ee-e20a-4049-8780-24c422bb3c90
+   *     Types: ZFA:0000107 # eye
+   *
+   * Individual: zfin:ZDB-GENE-000125-4 # dlc
+   *
+   * Individual: uuid:3e1ad895-56b2-4b54-a3f8-c99e7b42f646
+   *     Types: GO:0010467 # gene expression
+   *     Facts:
+   *         ps:associated_with_taxon  NCBITaxon:7955, # Danio rerio
+   *         dc:source                 zfin:ZDB-PUB-051025-1, 
+   *         BFO:0000066               uuid:033ab9ee-e20a-4049-8780-24c422bb3c90, # occurs_in that eye
+   *         ps:associated_with_gene   zfin:ZDB-GENE-000125-4 # dlc
+   *
+   * @return
+   */
   def translate(expressionLine: String): Set[OWLAxiom] = {
     val items = expressionLine.split("\t", -1)
     if (items(0).startsWith("ZDB-EFG")) {
